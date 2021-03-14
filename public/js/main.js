1d5519d719583b2e8115e7c1d50a2c1aa6573668
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-14 12:05:59 +0000

    show employer form when conditionId parameter is present

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+    const urlParameter = getUrlParameter("conditionId")
+    if (urlParameter) {
+        $('#candidate-salary-form').hide()
+        $('#employer-salary-form').show()
+    }
+})
+
 const salaryStandoffApiUrl = "http://localhost:8080"
 
 function handleCandidateSalaryForm(form) {
@@ -24,5 +32,21 @@ function handleCandidateSalaryForm(form) {
         }
     }
 
+    return false;
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
     return false;
 }
