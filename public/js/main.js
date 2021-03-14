eb537ea370f54766cffb9a9d687618bebab3f406
LLCampos <luis.filipe.lcampos@gmail.com>
2021-03-14 11:31:21 +0000

    log response to console

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,23 @@
-$(document).ready(function() {
-    $('#candidate-salary-form').ajaxForm(function() {
-        console.log("Submitted!")
-    })
-})
+function handleCandidateSalaryForm(form) {
+    var data = {};
+    for (var i = 0, ii = form.length; i < ii; ++i) {
+        var input = form[i];
+        if (input.name) {
+            data[input.name] = input.value;
+        }
+    }
+
+    // construct an HTTP request
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://localhost:8080/candidate_condition");
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.responseText)
+        }
+    }
+
+    return false;
+}
