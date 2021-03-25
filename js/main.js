7dd91ee198c1ed381ca48fa73dc3ab01898bf03f
Luís Campos <luis.filipe.lcampos@gmail.com>
2021-03-25 17:23:29 +0000

    Add condition metadata (#1)
    * post metadata
    
    * extra comment as text | send null if empty
    
    * add more currencies
    
    * improve form formatting
    
    * improve form formatting
    
    * change default currency
    
    * add placeholder text for extra comment input
    
    * remove unnecessary "required" flags
    
    * show metadata in employer salary form
    
    * error handling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
-    const urlParameter = getUrlParameter("conditionId")
-    if (urlParameter) {
+    const conditionId = getUrlParameter("conditionId")
+    if (conditionId) {
         $('#candidate-salary-form').hide()
-        $('#employer-salary-form').show()
+        showEmployerSalaryForm(conditionId)
     }
 })
 
+function showEmployerSalaryForm(conditionId) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${salaryStandoffApiUrl}/condition/metadata/${conditionId}`, true);
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                $('#employer-salary-form').show()
+                const jsonResp = JSON.parse(xhr.responseText)
+                $('#currency-employer-form').text(jsonResp.currency)
+                $('#gross-or-net-employer-form').text(jsonResp.grossOrNet)
+                $('#annual-or-monthly-employer-form').text(jsonResp.annualOrMonthly)
+                if (jsonResp.extraComments) {
+                    $('#extra-comments-div-employer-form').show()
+                    $('#extra-comments-employer-form').text(jsonResp.extraComments)
+                }
+            } else if (xhr.status === 404) {
+                $('#already-verified-badge').show()
+            } else {
+                $('#error-badge').show()
+            }
+        }
+    }
+}
+
+//const salaryStandoffApiUrl = "http://localhost:8080"
 const salaryStandoffApiUrl = "https://pacific-refuge-53323.herokuapp.com"
 
 function handleCandidateSalaryForm(form) {
@@ -19,7 +45,21 @@ function handleCandidateSalaryForm(form) {
         $('#error-badge').show()
     }
 
-    return handleForm(form, 'candidate_condition', 'candidate-salary-form', onSuccess, onFailure)
+    const formData = getFormData(form)
+    const json = JSON.stringify({
+        minSalaryAcceptable: formData.minSalaryAcceptable,
+        metadata: getMetadataFromFormData(formData)
+    })
+    return handleFormSubmit(json, 'candidate_condition', 'candidate-salary-form', onSuccess, onFailure)
+}
+
+function getMetadataFromFormData(formData) {
+    return {
+        currency: formData.currency,
+        grossOrNet: formData.grossOrNet,
+        annualOrMonthly: formData.annualOrMonthly,
+        extraComments: (formData.extraComments === "") ? null : formData.extraComments
+    }
 }
 
 function handleEmployerSalaryForm(form) {
@@ -42,15 +82,16 @@ function handleEmployerSalaryForm(form) {
         }
     }
 
+    const json = JSON.stringify(getFormData(form))
     const conditionId = getUrlParameter("conditionId")
-    return handleForm(form, `employer_condition/${conditionId}`, 'employer-salary-form', onSuccess, onFailure)
+    return handleFormSubmit(json, `employer_condition/${conditionId}`, 'employer-salary-form', onSuccess, onFailure)
 }
 
-function handleForm(form, endpoint, formId, onSuccess, onFailure) {
-    var xhr = new XMLHttpRequest();
+function handleFormSubmit(json, endpoint, formId, onSuccess, onFailure) {
+    const xhr = new XMLHttpRequest();
     xhr.open('POST', `${salaryStandoffApiUrl}/${endpoint}`);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify(getFormData(form)));
+    xhr.send(json);
 
     $("#" + formId).hide()
     $('#spinner').show()
